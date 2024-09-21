6990258225a750ad7cbc7a13fef7c20c2f0e162f
Savio Fernandes <savio.vincent@gmail.com>
2024-09-21 18:33:53 +0400

    Migrations added for Softwares

--- database/migrations/2024_09_16_120159_create_licenses_table.php
+++ database/migrations/2024_09_16_120159_create_licenses_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
             $table->id();
             $table->foreignId('software_id')->constrained()->onDelete('cascade');
             $table->string('license_type'); // e.g., 'subscription', 'perpetual', 'free'
+            $table->string('name')->nullable();  // License name, if applicable (Office 365, Antivirus, etc.)
+            $table->string('category')->nullable();  // Category, if relevant (Office, Antivirus, etc.)
+            $table->unsignedBigInteger('seats_available')->nullable();  // Number of allowed seats
+            $table->unsignedBigInteger('seats_used')->default(0);  // Number of seats in use
             $table->date('valid_from')->nullable();
             $table->date('valid_to')->nullable();
             $table->string('license_key')->nullable();
---
+++ database/migrations/2024_09_21_130335_create_computer_license_usage_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('computer_license_usage', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('computer_id')->constrained('computers')->onDelete('cascade');
+            $table->foreignId('license_id')->constrained('licenses')->onDelete('cascade');
+            $table->string('key')->nullable();  // License key, if applicable
+            $table->string('userid')->nullable();  // User ID, if applicable
+            $table->string('password')->nullable();  // Password, if applicable
+            $table->timestamp('assigned_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('computer_license_usage');
+    }
+};
